30f27c1d1af3af9bd359e229feae40938db742c6
Matthew Bunday <zenceph@gmail.com>
2014-04-17 17:41:48 -0400

    Playing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Prosedy::Application.routes.draw do
+  get "welcome/index"
   devise_for :authors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
