be9a0e47ba54e4e8f12fa0beb93b787745fa4271
Matthew Bunday <zenceph@gmail.com>
2014-04-19 19:18:10 -0400

    Persist article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render nothing: true
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
   end
 end
