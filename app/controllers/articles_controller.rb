d6d4c46dcc6448d49d0180a9069b8df70dd46d58
Matthew Bunday <zenceph@gmail.com>
2014-04-20 12:30:22 -0400

    Article show and index.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,14 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   def create
     @article = Article.new(article_params)
 
